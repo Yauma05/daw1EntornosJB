fa825a436080a1bae920dccb65c267ad17eec6f5
Jaume <jaubelbou@alu.edu.gva.es>
2026-01-16 14:01:39 +0100

    Prueba push

---
+++ holamundo.java
@@ -0,0 +1,9 @@
+//Aplicación holamundo de ejemplo
+
+class HolaMundoApp {
+	public static void main ( String args[])
+	{
+		System.out.println("Hola mundo!");
+		System.out.prinln("Hola Git!");
+	}
+}
